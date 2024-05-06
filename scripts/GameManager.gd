a67c6dd15fe56dbaac31b5a7610f84b798b54751
Alen Duda <alenduda@gmail.com>
2024-05-06 21:08:09 +0200

    Initial commit

---
+++ scripts/GameManager.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var score
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	score = 0
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
